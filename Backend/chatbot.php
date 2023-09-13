f0953e0f1cd5791ffd8ae6c181d5aa4d5c02dea3
LaIndependienteplat <144864511+LaIndependienteplat@users.noreply.github.com>
2023-09-12 20:12:06 -0600

    Add files via upload
    Add PHP script for handling user messages
    This commit introduces a PHP script responsible for handling user messages received as JSON input. The script decodes the JSON payload, checks for the presence of a user message, and interacts with a Python script for further processing. It also returns a response to the user.
    The script uses shell commands to manage Python processes and ensures a seamless conversation with the chat_function.py script.
    File: message_handler.php

---
+++ Backend/chatbot.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-type: application/json; charset=utf-8");
+$json = file_get_contents('php://input');
+$mensaje = json_decode($json);
+$mensajeUsuario = $mensaje->{'mensaje'};
+$userid = $mensaje->{'userid'};
+
+if($mensajeUsuario == '')
+{
+  echo json_encode('mensaje no recibido');
+}
+else
+{
+  $proccess = "pgrep python";
+  $result_process = shell_exec($proccess);
+  $flag = True;
+
+  while(strlen($result_process) == 0 && $flag)
+  {
+    // activar variable env conda
+    $run_ada = '/opt/anaconda3/bin/python chat_function.py "'.$mensajeUsuario.'" "'.$userid;
+    $run_command = shell_exec($run_ada);
+    $flag = False;
+  }
+  ////////////////Ya se lee y devuelve mensaje de usuario
+  $response = explode("Reply: ", $run_command);
+   echo json_encode($response[1]);
+}
+?>
